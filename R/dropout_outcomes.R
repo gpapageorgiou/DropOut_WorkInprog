0719350b78a70f6d4740d9819ba2f1cfdc857631
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-06-13 11:50:49 +0200

    Function to create combinations of dropout outcomes

---
+++ R/dropout_outcomes.R
@@ -0,0 +1,25 @@
+dropout_outcomes <- function(dat) {
+  sumdinum <- sum(dat$d_i.num)
+  if (sumdinum > 0) {
+    dat$di_all <- 1
+  } else {
+    dat$di_all <- 0
+  }
+  sumdeath <- sum(dat$death)
+  if (all(sumdeath == 0, sumdinum > 0)) {
+    dat$di_all_cens <- 1
+  } else {
+    dat$di_all_cens <- 0
+  }
+  if (any(sumdinum > 0, sumdeath > 0)) {
+    dat$di_all_death <- 1
+  } else {
+    dat$di_all_death <- 0
+  }
+  if (sumdeath > 0) {
+    dat$di_onlydeath <- 1
+  } else {
+    dat$di_onlydeath <- 0
+  }
+  dat
+}
