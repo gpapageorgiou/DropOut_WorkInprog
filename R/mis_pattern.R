4e124924fee6372d4aeeb94720c0b6f1af28c191
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-29 15:50:25 +0200

    Added mis_pattern function

---
+++ R/mis_pattern.R
@@ -0,0 +1,6 @@
+mis_pattern <- function(dat.id, planned = c(0, 2, 6, 12, 18), 
+                        timevar = 'obstime') {
+  pattern <- as.numeric(!planned %in% dat.id[[timevar]])
+  pattern <- ifelse(pattern == 0, 'O', 'X')
+  paste(pattern, collapse = "")
+}
