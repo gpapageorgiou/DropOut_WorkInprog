0963bb410100456a39dd69619afd6b5bba55ba53
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-29 16:29:40 +0200

    Corrected typo

--- R/mis_pattern.R
+++ R/mis_pattern.R
@@ -3,6 +3,6 @@ mis_pattern <- function(dat.id, planned = c(0, 2, 6, 12, 18),
   pattern <- as.numeric(!planned %in% dat.id[[timevar]])
   pattern <- ifelse(pattern == 0, 'O', 'X')
   pattern <- paste(pattern, collapse = "")
-  dat$pattern <- pattern
-  dat
+  dat.id$pattern <- pattern
+  dat.id
 }
