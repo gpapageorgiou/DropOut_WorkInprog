4becc84f916424123d931a908535668dfd669299
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-07-29 16:26:35 +0200

    Corrected return for mis_pattern

--- R/mis_pattern.R
+++ R/mis_pattern.R
@@ -2,5 +2,7 @@ mis_pattern <- function(dat.id, planned = c(0, 2, 6, 12, 18),
                         timevar = 'obstime') {
   pattern <- as.numeric(!planned %in% dat.id[[timevar]])
   pattern <- ifelse(pattern == 0, 'O', 'X')
-  paste(pattern, collapse = "")
+  pattern <- paste(pattern, collapse = "")
+  dat$pattern <- pattern
+  dat
 }
